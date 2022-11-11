cf76267e3a4e4ed2f4b3c27ec362aaca694cfab8
Mohammad Obada Fadloun <34578532+obadafa@users.noreply.github.com>
2022-11-11 06:53:24 +0200

    First Commit

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Bsckdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Model = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Bsckdrop onClick={props.onClick} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Model;
